dc91b93e4c986ea1ea3756dfd8672d3af70fe108
rjtmhy <rjtmhy@gmail.com>
2018-11-10 11:04:37 +0530

    new File2.c and edit File1.c

--- File2.c
+++ File2.c
@@ -1,17 +1,8 @@
 #include<stdio.h>
 
-
-void func()
-{
-for(int i=1;i<3;i++)
-{
-printf("%d",i*2);
-}
-}
-
 int main()
 {
 printf("Hello World!\n");
-func();
+printf("World needs to be more disciplined and caring!");
 return 0;
 }
